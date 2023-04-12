38249d55f7b001bc8b2b763fcf3f7db033d5769b
litvinovmitch11 <mihaillitvinov26324@gmail.com>
2023-04-12 05:41:20 +0300

    NEW REQUESTS WOW... and debug main client

--- requests/main_client.py
+++ requests/main_client.py
@@ -2,6 +2,9 @@ import grpc
 import starter_pb2 as pb2
 import starter_pb2_grpc as pb2_grpc
 
+from all_include import Caste
+from model import ControlToken
+
 
 class Client(object):
 
@@ -19,23 +22,78 @@ class Client(object):
         print("AddPlayer")
         return self.stub.AddPlayer(pb2.PlayerId(player_id=player_id))
 
+    def swap_player_readiness_value(self, player_id):
+        print("SwapPlayerReadinessValue")
+        return self.stub.SwapPlayerReadinessValue(pb2.PlayerId(player_id=player_id))
+
+    def get_free_caste(self):
+        print("GetFreeCaste")
+        return self.stub.GetFreeCaste(pb2.Empty())
+
+    def set_caste(self, player_id, caste):
+        print("SetCaste")
+        return self.stub.SetCaste(pb2.Caste(player_id=player_id, caste=caste))
+
+    def get_possible_positions_control_token(self):
+        print("GetPossiblePositionsControlToken")
+        return self.stub.GetPossiblePositionsControlToken(pb2.Empty())
+
+    def put_control_token(self, player_id, control_token, province_id):
+        print("PutControlToken")
+        token = pb2.ControlToken(visible=control_token.visible, on_board=control_token.on_board,
+                                 power=control_token.power, caste=str(control_token.caste))
+        return self.stub.PutControlToken(
+            pb2.AddControlToken(player_id=player_id, control_token=token, province_id=province_id))
+
+    def round_count(self):
+        print("RoundCount")
+        return self.stub.RoundCount(pb2.Empty())
+
 
 if __name__ == '__main__':
     client = Client()
 
-    your_id = client.get_unique_id().player_id
-    print(f'Your ID: {your_id}')
+    # Debug client...
+    # cmd - number of command from starter facade (or command name)
+    # If you want to break client write -1
 
-    key = client.add_player(your_id)
-    if key:
-        print("Good!")
-    else:
-        print("Your id has been added :(")
+    your_id = -1
+    cmd = input()
+    while cmd != '-1':
+        if cmd == '1' or cmd == 'get_unique_id':
+            your_id = client.get_unique_id().player_id
+            print(f'Your id: {your_id}')
+        elif (cmd == '2' or cmd == 'add_player') and your_id != -1:
+            key = client.add_player(your_id).key
+            print(f'Add player with id {your_id}\nIs ok? {key}')
+        elif (cmd == '3' or cmd == "swap_player_readiness_value") and your_id != -1:
+            key = client.swap_player_readiness_value(your_id).key
+            print(f'Readiness player with id {your_id}: {key}')
+        elif cmd == '4' or cmd == "get_free_caste":
+            free_caste = client.get_free_caste().caste
+            print(f'Free caste: {free_caste}')
+        elif (cmd == '5' or cmd == 'set_caste') and your_id != -1:
+            your_caste = input("Enter caste: ")
+            key = client.set_caste(your_id, your_caste).key
+            print(f'Correct? {key}')
+        elif cmd == '6' or cmd == "get_possible_positions_control_token":
+            list_token = client.get_possible_positions_control_token().token
+            print(f'List of possible position control tokens: {list_token}')
+        elif (cmd == '7' or cmd == "put_control_token") and your_id != -1:
+            print("Enter control token parameters and province id: ")
+            visible = bool(input("Visible: "))
+            on_board = bool(input("On_board: "))
+            power = int(input("Power: "))
+            your_caste = input("Caste: ")
+            your_province_id = int(input("Province id: "))
 
-    key2 = client.add_player(your_id)
-    if key2:
-        print("Good!")
-    else:
-        print("Your id has been added :(")
+            your_token = ControlToken(Caste(your_caste), power)
+            your_token.visible = visible
+            your_token.on_board = on_board
 
-    print(f'Your ID: {your_id}')
+            key = client.put_control_token(your_id, your_token, your_province_id).key
+            print(f'Is correct? {key}')
+        elif cmd == '8' or cmd == 'round_count':
+            print(f'Current round is {client.round_count().round}')
+
+        cmd = input()
--- requests/starter_service.py
+++ requests/starter_service.py
@@ -2,6 +2,9 @@ import starter_pb2 as pb2
 import starter_pb2_grpc as pb2_grpc
 from facade import StarterFacade
 
+from all_include import Caste
+from model import ControlToken
+
 
 class StarterService(pb2_grpc.StarterServicer):
 
@@ -15,12 +18,40 @@ class StarterService(pb2_grpc.StarterServicer):
 
     def AddPlayer(self, request, context):
         print("AddPlayer")
-        key = self.facade.add_player(request.player_id)
-        result = {"key": key}
+        result = {"key": self.facade.add_player(request.player_id)}
         return pb2.Key(**result)
 
-    # def get_free_caste(self, request, context):
-    #     print("GetFreeCaste")
-    #     list_caste = pb2.ListCaste()
-    #     list_caste.caste = list(map(str, self.facade.get_free_caste()))
-    #     return list_caste
+    def SwapPlayerReadinessValue(self, request, context):
+        print("SwapPlayerReadinessValue")
+        result = {"key": self.facade.swap_player_readiness_value(request.player_id)}
+        return pb2.Key(**result)
+
+    def GetFreeCaste(self, request, context):
+        print("GetFreeCaste")
+        list_caste = pb2.ListCaste()
+        list_caste.caste[:] = [caste.value for caste in self.facade.get_free_caste()]
+        return list_caste
+
+    def SetCaste(self, request, context):
+        print("SetCaste")
+        result = {"key": self.facade.set_caste(request.player_id, Caste(request.caste))}
+        return pb2.Key(**result)
+
+    def GetPossiblePositionsControlToken(self, request, context):
+        print("GetPossiblePositionsControlToken")
+        list_tokens = pb2.ListTokens()
+        list_tokens.token[:] = self.facade.get_possible_positions_control_token()
+        return list_tokens
+
+    def PutControlToken(self, request, context):
+        print("PutControlToken")
+        token = ControlToken(Caste(request.ControlToken.caste), request.ControlToken.power)
+        token.visible = request.ControlToken.visible
+        token.on_board = request.ControlToken.on_board
+        result = {"key": self.facade.put_control_token(request.player_id, token, request.province_id)}
+        return pb2.Key(**result)
+
+    def RoundCount(self, request, context):
+        print("RoundCount")
+        result = {"round": self.facade.round_count()}
+        return pb2.Round(**result)
