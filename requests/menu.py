68e6b7b732ccdb3ddedbb0fd795674db1f6c9715
litvinovmitch11 <mihaillitvinov26324@gmail.com>
2023-04-13 10:49:55 +0300

    some pretty fix

--- requests/menu.py
+++ requests/menu.py
@@ -1,20 +1,16 @@
-from pygame import *
-from main_client import *
-import grpc
-init()
+import pygame as pg
+from main_client import Client
 
-size = (800, 600)
-screen = display.set_mode(size)
-ARIAL_50 = font.SysFont('arial', 50)
 
 class Menu:
     def __init__(self):
         self._option_surfaces = []
         self._callbacks = []
         self._current_option_index = 0
+        self.ARIAL_50 = pg.font.SysFont('arial', 50)
 
     def append_option(self, option, callback):
-        self._option_surfaces.append(ARIAL_50.render(option, True, (255, 255, 255)))
+        self._option_surfaces.append(self.ARIAL_50.render(option, True, (255, 255, 255)))
         self._callbacks.append(callback)
 
     def switch(self, direction):
@@ -28,35 +24,38 @@ class Menu:
             option_rect = option.get_rect()
             option_rect.topleft = (x, y + i * option_y_padding)
             if i == self._current_option_index:
-                draw.rect(surf, (0, 100, 0), option_rect)
+                pg.draw.rect(surf, (0, 100, 0), option_rect)
             surf.blit(option, option_rect)
-client = Client()
-menu = Menu()
-menu.append_option('Add Player', client.add_player(client.get_unique_id()))
-menu.append_option('Get Unique Id', client.get_unique_id())
-menu.append_option('Get Ready', client.swap_player_readiness_value(client.get_unique_id()))
 
 
-menu.append_option('Quit', quit)
+if __name__ == '__main__':
+    pg.init()
+    size = (800, 600)
+    screen = pg.display.set_mode(size)
 
+    client = Client()
+    menu = Menu()
 
+    your_id = client.get_unique_id().player_id
+    menu.append_option('Add Player', client.add_player(your_id))
+    menu.append_option('Get Ready', client.swap_player_readiness_value(your_id))
+    menu.append_option('Quit', quit)
 
-running = True
-while running:
-    for e in event.get():
-        if e.type == QUIT:
-            running = False
-        elif e.type == KEYDOWN:
-            if e.key == K_w:
-                menu.switch(-1)
-            elif e.key == K_s:
-                menu.switch(1)
-            elif e.key == K_SPACE:
-                menu.select()
+    running = True
+    while running:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                running = False
+            elif e.type == pg.KEYDOWN:
+                if e.key == pg.K_w:
+                    menu.switch(-1)
+                elif e.key == pg.K_s:
+                    menu.switch(1)
+                elif e.key == pg.K_SPACE:
+                    menu.select()
 
-    screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))
+        menu.draw(screen, 100, 100, 75)
+        pg.display.flip()
 
-    menu.draw(screen, 100, 100, 75)
-
-    display.flip()
-quit()
+    pg.quit()
