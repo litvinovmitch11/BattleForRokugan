b6efb9e5cad1fe3e17875fdba8ab38b4f757e8ca
Lokoner <93461114+Lokoner@users.noreply.github.com>
2023-05-30 22:56:23 +0300

    Update map.py

--- requests/map.py
+++ requests/map.py
@@ -1,8 +1,8 @@
 import pygame
 
-
 class Map:
-    def __init__(self, screen):
+
+    def __init__(self,screen):
         self.screen = screen
         self.image = pygame.image.load('BattleForRokugan_content/MAP31.jpg')
         self.image = pygame.transform.rotate(self.image, 90)
@@ -10,7 +10,31 @@ class Map:
         self.screen_rect = screen.get_rect()
         self.rect_centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        self.rect = pygame.Rect(250, 0, 600, 600)
+        self.rect = pygame.Rect(250, 20, 600, 600)
+
+    def output(self):
+        self.screen.blit(self.image, self.rect)
+
+
+class PlayersAbuility:
+    def __init__(self, screen, caste):
+        self.screen = screen
+        self.caste = caste
+        if caste == "unicorn":
+            self.image = pygame.image.load('BattleForRokugan_content/unic-shirm.png')
+        elif caste == "crab":
+            self.image = pygame.image.load('BattleForRokugan_content/??')
+        elif caste == "crane":
+            self.image = pygame.image.load('BattleForRokugan_content/??')
+        elif caste == "lion":
+            self.image = pygame.image.load('BattleForRokugan_content/??')
+        elif caste == "scorpion":
+            self.image = pygame.image.load('BattleForRokugan_content/??')
+        elif caste == "dragon":
+            self.image = pygame.image.load('BattleForRokugan_content/??')
+        elif caste == "phoenix":
+            self.image = pygame.image.load('BattleForRokugan_content/??')
+        self.rect = pygame.Rect(1000, 700, 200, 200)
 
     def output(self):
         self.screen.blit(self.image, self.rect)
