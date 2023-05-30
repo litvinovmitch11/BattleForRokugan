c4ace3f809cfc85ed7296e99d9aed22f27c9c15d
Lokoner <93461114+Lokoner@users.noreply.github.com>
2023-05-30 20:33:32 +0300

    Update start_game.py

--- requests/start_game.py
+++ requests/start_game.py
@@ -4,9 +4,8 @@ from pygame.locals import *
 
 from map import Map
 from tokens import control_token, battle_token
-
-
 def run():
+
     pygame.init()
 
     WIN_WIDTH = 1540  # Ширина создаваемого окна
@@ -14,8 +13,8 @@ def run():
     DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и высоту в одну переменную
 
     screen = pygame.display.set_mode((500, 500), HWSURFACE | DOUBLEBUF | RESIZABLE)
-    pygame.display.set_caption("Battle For Rokugan")
-    backgroung_color = (255, 255, 255)
+    pygame.display.set_caption("Battle For Rakugan")
+    backgroung_color = (255,255,255)
     background_image = pygame.image.load('BattleForRokugan_content/bg.jpg')
     mapp = Map(screen)
 
@@ -32,21 +31,23 @@ def run():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        # screen.fill(backgroung_color)
-        # bg = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
-        # будем использовать как фон
         mapp.output()
 
-        CT1 = control_token(screen, "dragon", 1, 8)
+        CT1 = control_token(screen, "dragon", 1, 29)
         CT1.output()
         CT2 = control_token(screen, "unicorn", 1, 6)
         CT2.output()
         CT3 = control_token(screen, "crab", 0, 3)
         CT3.output()
 
+        BT1 = battle_token(screen, "crane", 1, "infantry", 1, 30, 0)
+        BT1.output()
+        BT2 = battle_token(screen, "unicorn", 1, "infantry", 1, 0, 1)
+        BT2.output()
+
         pygame.display.flip()
 
-        # screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
+        #screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
         pygame.display.update()  # обновление и вывод всех изменений на экран
 
-# run()
+#run()
