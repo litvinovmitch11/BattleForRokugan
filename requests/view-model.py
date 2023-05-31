66025e14f20eca1f3108864f2ee3589869870b15
gabik <gabidullinkamil1@gmail.com>
2023-05-31 18:01:28 +0300

    changes in view-model

--- requests/view-model.py
+++ requests/view-model.py
@@ -1,37 +1,48 @@
 from all_include import *
 from main_client import Client
+import random
 
 
 class BattleToken:
 
-    def __init__(self, bt_ind: int, prov_from: int, prov_to: int, visible: bool, caste: Caste):
+    def __init__(self, bt_ind: int, prov_from: int, prov_to: int, visible: bool, new_caste: Caste, in_reset: bool,
+                 in_active: bool, my_type: int, power: int):
         self.ind = bt_ind
         self.prov_from = prov_from
         self.prov_to = prov_to
         self.visible = visible
-        self.caste = Caste
+        self.caste = Caste(new_caste)
+        self.in_reset = in_reset
+        self.in_active = in_active
+        self.type = TokenType(my_type)
+        self.power = power
 
-    def change_all_val(self, bt_ind: int, prov_from: int, prov_to: int, visible: bool, caste: Caste):
+    def change_all_val(self, bt_ind: int, prov_from: int, prov_to: int, visible: bool, new_caste: Caste, in_reset: bool,
+                       in_active: bool, my_type: int, power: int):
         self.ind = bt_ind
         self.prov_from = prov_from
         self.prov_to = prov_to
         self.visible = visible
-        self.caste = Caste
+        self.caste = Caste(new_caste)
+        self.in_reset = in_reset
+        self.in_active = in_active
+        self.type = TokenType(my_type)
+        self.power = power
 
 
 class ControlToken:
 
-    def __init__(self, t_ind: int, prov: int, visible: bool, caste: Caste):
+    def __init__(self, t_ind: int, prov: int, visible: bool, new_caste: Caste):
         self.ind = t_ind
         self.prov = prov
         self.visible = visible
-        self.caste = Caste
+        self.caste = Caste(new_caste)
 
-    def change_all_val(self, t_ind: int, prov: int, visible: bool, caste: Caste):
+    def change_all_val(self, t_ind: int, prov: int, visible: bool, new_caste: Caste):
         self.ind = t_ind
         self.prov = prov
         self.visible = visible
-        self.caste = Caste
+        self.caste = Caste(new_caste)
 
 
 class Player:  # info, for client
@@ -43,6 +54,9 @@ class Player:  # info, for client
         self.my_ind = player_ind
         self.name = name
 
+    def set_caste(self, new_caste: Caste):
+        self.caste = new_caste
+
 
 class Game:
 
@@ -52,6 +66,8 @@ class Game:
         self.round = 0
         self.battle_tokens = dict()  # id -> Class BattleToken
         self.control_tokens = dict()  # id -> Class ControlTokens
+
+        self.whose_move = -1  # WARNING
 
     def add_player(self, player_ind: int, name: str):
         self.players[player_ind] = Player(player_ind, name)
@@ -73,8 +89,11 @@ if __name__ == '__main__':
         players = client.get_players_ids(gm.ind).int
     print(players)
     for i in players:
-        print(client.get_free_caste(gm.ind).caste)
-        client.set_caste(i, client.get_free_caste(gm.ind).caste[0], gm.ind)
+        my_caste = client.get_free_caste(gm.ind).caste[0]
+        if client.set_caste(i, my_caste, gm.ind):
+            caste = Caste(my_caste)
+            gm.players[i].set_caste(caste)
+
     print(client.round_count(gm.ind).round)
     tokens = client.get_all_battle_token(gm.ind).token
     for token in tokens:
@@ -104,10 +123,12 @@ if __name__ == '__main__':
         for token in battle_tokens:
             if token.id not in gm.battle_tokens:
                 gm.battle_tokens[token.id] = BattleToken(token.id, token.on_board_first, token.on_board_second,
-                                                          token.visible, token.caste)
+                                                         token.visible, token.caste, token.in_reset, token.in_active,
+                                                         token.type, token.power)
             else:
                 gm.battle_tokens[token.id].change_all_val(token.id, token.on_board_first, token.on_board_second,
-                                                          token.visible, token.caste)
+                                                          token.visible, token.caste, token.in_reset, token.in_active,
+                                                          token.type, token.power)
         gm.round = client.round_count(gm.ind).round
         print(gm.round)
         break
@@ -117,4 +138,11 @@ if __name__ == '__main__':
         for i in range(10):
             for id_player in players:
                 client.unused_card(id_player, gm.ind)
-
+        while client.get_phase(gm.ind).round == 2:
+            f = random.randint(0, 29)
+            t = random.randint(0, 29)
+            was = False
+            for id_player in players:
+                active = []
+                # for token in gm.battle_tokens:
+                #     if token.caste == gm.players[id_player].caste:
