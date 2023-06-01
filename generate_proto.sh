50c9d20b2ae1aa2af4cc6d007507af90840642ca
litvinovmitch11 <mihaillitvinov26324@gmail.com>
2023-06-01 23:06:21 +0300

    delete starter files

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Run from root directory (change 'python' to 'python3' for linux)
-# python -m grpc_tools.protoc -I ./protos --python_out=./requests --grpc_python_out=./requests ./protos/starter.proto
 python -m grpc_tools.protoc -I ./protos --python_out=./requests --grpc_python_out=./requests ./protos/facade.proto
