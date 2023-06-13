a14e026bbe5bab43d23301726faf4d2430eb2a8e
litvinovmitch11 <mihaillitvinov26324@gmail.com>
2023-06-14 02:16:19 +0300

    add upd generate_proto.sh

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc -I ../proto ./ --python_out=../common --grpc_python_out=../common ./facade.proto
-python -m grpc_tools.protoc -I ../proto --python_out=../common --grpc_python_out=../common ./registration.proto
+python -m grpc_tools.protoc -I ../proto --python_out=../common --grpc_python_out=../common ../proto/facade.proto
+python -m grpc_tools.protoc -I ../proto --python_out=../common --grpc_python_out=../common ../proto/registration.proto
