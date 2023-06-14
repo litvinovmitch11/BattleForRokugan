897f6b12cc493f1edb47c5149a3915141cdac2c6
litvinovmitch11 <mihaillitvinov26324@gmail.com>
2023-06-14 07:10:14 +0300

    upd generate proto

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # if it doesn't work on ubuntu try change python to python3
+if ! [ -d ../common ]; then
+mkdir '../common'
+fi
 python -m grpc_tools.protoc -I ../proto --python_out=../common --grpc_python_out=../common ../proto/facade.proto
 python -m grpc_tools.protoc -I ../proto --python_out=../common --grpc_python_out=../common ../proto/registration.proto
