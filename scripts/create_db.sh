2eb7677e068661229becd4cc05a4043d7af679dc
litvinovmitch11 <mihaillitvinov26324@gmail.com>
2023-06-14 03:46:58 +0300

    upd create_db.sh

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -6,3 +6,11 @@ fi
 PGPASSWORD=postgres psql -U postgres -c "CREATE DATABASE bfr"
 PGPASSWORD=postgres psql -U postgres -d bfr -c "CREATE SCHEMA client"
 PGPASSWORD=postgres psql -U postgres -d bfr -c "CREATE TABLE client.players(login varchar(200), key varchar(200), salt varchar(200), games_cnt int, wins_cnt int)"
+
+# if doesn't work on ubuntu, try:
+#if sudo -u postgres psql -U postgres -lqt | cut -d \| -f 1 | grep -qw bfr; then
+#  sudo -u postgres psql -U postgres -c "DROP DATABASE bfr"
+#fi
+#sudo -u postgres psql -U postgres -c "CREATE DATABASE bfr"
+#sudo -u postgres psql -U postgres -d bfr -c "CREATE SCHEMA client"
+#sudo -u postgres psql -U postgres -d bfr -c "CREATE TABLE client.players(login varchar(200), key varchar(200), salt varchar(200), games_cnt int, wins_cnt int)"
